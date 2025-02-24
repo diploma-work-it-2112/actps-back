050ac314df10c8e73bf8de182a7b41d7491115a2
D-Diyazzz <diyazzz.dev@gmail.com>
2025-02-24 21:11:41 +0500

    add trafic_log table to db

--- src/actps/integrations/postgres/tables.py
+++ src/actps/integrations/postgres/tables.py
@@ -29,6 +29,24 @@ router = Table(
     Column("id", BigInteger, primary_key=True, index=True, autoincrement=True),
     Column("model_name", String(256), nullable=False),
     Column("ip_address", String(256), nullable=False),
+    Column("color", String(8), nullable=False),
     Column("hostname", String(256), nullable=False),
     Column("created_at", DateTime(timezone=True), nullable=False)
 )
+
+package_log = Table(
+    "package_log",
+    mapper_registry.metadata,
+    Column("id", BigInteger, primary_key=True, index=True, autoincrement=True),
+    Column("message", String(512), nullable=True),
+    Column("ip_source", String(256), nullable=False),
+    Column("ip_destination", String(256), nullable=False),
+    Column("mac_source", String(256), nullable=False),
+    Column("mac_destination", String(256), nullable=False),
+    Column("port_source", String(256), nullable=False),
+    Column("port_destination", String(256), nullable=False),
+    Column("host_name", String(256), nullable=True),
+    Column("time", DateTime(timezone=True), nullable=False),
+    Column("created_at", DateTime(timezone=True), nullable=True)
+)
+
