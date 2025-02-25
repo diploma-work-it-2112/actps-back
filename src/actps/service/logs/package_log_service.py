cbfaee8c9cc3a22debf2327cde1007fb109b5fd6
D-Diyazzz <diyazzz.dev@gmail.com>
2025-02-25 13:22:52 +0500

    fix problem with creating obj in package log service

--- src/actps/service/logs/package_log_service.py
+++ src/actps/service/logs/package_log_service.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 
 from src.actps.core.unit_of_work import UnitOfWork 
 from src.actps.domain.logs import PackageLog 
+from src.actps.domain.pc import PC
 
 
 class PackageLogService:
@@ -11,22 +12,25 @@ class PackageLogService:
         data: BaseModel,
         uow: UnitOfWork
     ) -> PackageLog:
-        package_log = PackageLog(
-            ip_source=data.ip_source,
-            ip_destination=data.ip_destination,
-            mac_source=data.mac_source,
-            mac_destination=data.mac_destination,
-            port_source=data.port_source,
-            port_destination=data.port_destination,  
-            time=data.time,
-            web_host_name=data.web_host_name,
-            pc_id=data.pc_id,
-            message=data.message,
-            id=data.id,
-            created_at=data.created_at
-        )
 
         async with uow as uow:
+            pc_repository = await uow.get_repository(PC)
+            pc_id = await pc_repository.get_by_hostname(data.pc_hostname)
+            package_log = PackageLog(
+                ip_source=data.ip_source,
+                ip_destination=data.ip_destination,
+                mac_source=data.mac_source,
+                mac_destination=data.mac_destination,
+                port_source=data.port_source,
+                port_destination=data.port_destination,  
+                time=data.time,
+                web_host_name=data.web_host_name,
+                pc_id=pc_id,
+                message=data.message,
+                id=data.id,
+                created_at=data.created_at
+            )
+
             await uow.add(package_log)
             await uow.commit()
 
