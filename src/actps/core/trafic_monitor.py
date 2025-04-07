f423ba24064261ae58dc00b4a92b59010ad5fe68
D-Diyazzz <diyazzz.dev@gmail.com>
2025-04-07 18:48:48 +0500

    add abstract packet parse

--- src/actps/core/trafic_monitor.py
+++ src/actps/core/trafic_monitor.py
@@ -5,22 +5,63 @@ class AbstractTraficMonitoring(ABC):
 
     @abstractmethod
     def get_logs(self):
-        return NotImplementedError
+        raise NotImplementedError
 
     @abstractmethod
     def write_logs(self, logs):
-        return NotImplementedError
+        raise NotImplementedError
 
     @abstractmethod
     def parce_package(self, packet):
-        return NotImplementedError
+        raise NotImplementedError
 
     @abstractmethod
     def monitor(self, packet):
-        return NotImplementedError
+        raise NotImplementedError
 
     @abstractmethod
     def run(self):
-        return NotImplementedError
+        raise NotImplementedError
 
 
+class AbstractTraficParser(ABC):
+
+    @abstractmethod
+    def parce(self, packet):
+        raise NotImplementedError 
+
+    @abstractmethod
+    def ether_parse(self, packet):
+        raise NotImplementedError
+
+    @abstractmethod
+    def ip_parse(self, packet):
+        raise NotImplementedError
+
+    @abstractmethod
+    def tcp_parse(self, packet):
+        raise NotImplementedError
+
+    @abstractmethod
+    def udp_parse(self, packet):
+        raise NotImplementedError
+
+    @abstractmethod
+    def icmp_parse(self, packet):
+        raise NotImplementedError
+
+    @abstractmethod
+    def ipv6_parce(self, packet):
+        raise NotImplementedError
+
+    @abstractmethod
+    def arp_parce(self, packet):
+        raise NotImplementedError
+
+    @abstractmethod
+    def http_parse(self, packet):
+        raise NotImplementedError
+
+    @abstractmethod
+    def dns_parse(self, packet):
+        raise NotImplementedError
