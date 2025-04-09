1c83893c8cf4e69dd79d36bd09168a6cebd9e43d
D-Diyazzz <diyazzz.dev@gmail.com>
2025-04-09 13:35:45 +0500

    add websocket connection to get trafic packets

--- src/actps/gateway/handler/trafic_monitoring_handler.py
+++ src/actps/gateway/handler/trafic_monitoring_handler.py
@@ -1,3 +1,5 @@
+import json
+
 from fastapi import WebSocket, WebSocketDisconnect
 
 from src.actps.integrations.redis.redis import RedisCacheService
@@ -7,5 +9,20 @@ redis_trafic_monitor_session = RedisCacheService(db=2)
 
 
 async def monitor_trafic_handler(ws: WebSocket):
-    pass
+    await ws.accept()
+    try:
+        first_id = None
+        while True: 
+            data = redis_trafic_monitor_session.xrange(stream_key="trafic_logs_key", min="-", max="+", count=50)
+            count = 0
+            json_data = json.dumps(data)
+            for id, fields in data:
+                if count == 0 and first_id == id:
+                    break
+                elif count == 0:
+                    first_id = id
+                json_data = json.dumps(fields)
+                await ws.send_text(json_data)
+    except: 
+        pass
 
